447e58c6c6f440f15e81c86b7e67ef40fb79112f
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2021-04-02 15:58:46 -0500

    feat(responsivebar): height and width set to 100%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Multibar from './components/Multibar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="row">
+        <div className="col-lg-2 col-md-2 col-sm-2">
+        <p>hello</p>
+        </div>
+        <div className="col-lg-10 col-md-10 col-sm-10" style={{height:'500px'}}>
+        <Multibar/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Multibar.js
@@ -0,0 +1,101 @@
+// STEP 1 - Include Dependencies
+
+// Include react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Column2D from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+// STEP 2- Define the categories representing the labels on the X-axis
+const categories =  [
+    {
+      "category": [
+        { "label": "Q1" },
+        { "label": "Q2" },
+        { "label": "Q3" },
+        { "label": "Q4" },
+        { "label": "Q1" },
+        { "label": "Q2" },
+        { "label": "Q3" },
+        { "label": "Q4" }
+      ]
+    }
+  ]
+  // STEP 3- Construct the dataset comprising multiple series
+  const dataset = [
+    {
+      "seriesname": "Previous Year",
+      "data": [
+        { "value": "1" },
+        { "value": "2" },
+        { "value": "5" },
+        { "value": "16" },
+        { "value": "1" },
+        { "value": "2" },
+        { "value": "5" },
+        { "value": "16" }
+      ]
+    },
+    {
+      "seriesname": "Current Year",
+      "data": [
+        { "value": "20" },
+        { "value": "2" },
+        { "value": "20" },
+        { "value": "22" },
+        { "value": "20" },
+        { "value": "2" },
+        { "value": "20" },
+        { "value": "22" }
+      ]
+    }
+  ]
+
+// STEP 4 - Creating the JSON object to store the chart configurations
+const chartConfigs = {
+    type: 'mscolumn2d',// The chart type
+    width: '100%', // Width of the chart
+    height: '100%', // Height of the chart
+    dataFormat: 'json', // Data type
+     dataSource: {
+    //Chart Configurations
+      "chart": {
+        "theme": "fusion",
+        "caption": "Comparison of Quarterly Revenue",
+        "xAxisname": "Quarter",
+        "yAxisName": "Revenues (In USD)",
+        "numberPrefix": "$",
+        "plotFillAlpha": "80",
+        "divLineIsDashed": "1",
+        "divLineDashLen": "1",
+        "divLineGapLen": "1"
+      },
+      "categories": categories,
+      "dataset": dataset,
+    }
+  }
+// STEP 5 - Creating the DOM element to pass the react-fusioncharts component
+class Multibar extends React.Component {
+  render() {
+     return (
+     <ReactFC
+        {...chartConfigs}/>
+
+     );
+  }
+}
+
+export default Multibar;
